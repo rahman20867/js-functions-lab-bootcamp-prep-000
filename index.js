02bc79f4c362484579e900fc771d3220fe2bf09c
Marvin Andrade <marvin@lionguardsecurity.com>
2017-10-31 01:35:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!"
+   "Happy holidays, ${name}!"
 }
 function happyHolidayTo(holiday, name){
- return  "Happy ${holiday}, ${name}!"
+   "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown(holiday, days) {
- return  "It\'s ${days} days until ${holiday}!"
+ "It\'s ${days} days until ${holiday}!"
 
 }
